0ad2473dad34952518d61b3303d45ddd5762d9da
Jack Crish <jcrish@gmail.com>
2022-09-20 22:47:27 -0400

    react success!!!

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,28 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { babel } from '@rollup/plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
+import replace from '@rollup/plugin-replace';
+
 
 export default [
   {
-    input: ["src/main.js"],
+    input: ["src/index.js"],
     output : {
       file: "dist/bundle.js", 
       format: "iife", 
       name:"main",
-      sourcemap: true
+      sourcemap: true,
     },
     plugins: [
-      nodeResolve({extensions: [".js"]}),
-      commonjs(),
+      replace({
+        'process.env.NODE_ENV': '"development"'
+      }),
+      nodeResolve({
+        extensions: ['.js', '.jsx']
+      }),
+      commonjs({
+        include: ['node_modules/**']
+      }),
       babel({
         exclude: 'node_modules/**',
         babelHelpers: 'bundled',
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {App} from "./App.jsx";
+import { createRoot } from 'react-dom/client';
+import { App } from "./App.jsx";
 
-
-ReactDOM.render(<App />, document.querySelector('#root'));
+  const domContainer = document.querySelector('#root');
+  const root = createRoot(domContainer);
+  root.render(<App />);
